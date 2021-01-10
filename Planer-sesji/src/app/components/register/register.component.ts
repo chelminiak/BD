73dc61a4844ae3db21dc719863e87083583bdff1
chelminiak <chelminiak@student.agh.edu.pl>
2021-01-10 17:49:46 +0100

    validators update

--- Planer-sesji/src/app/components/register/register.component.ts
+++ Planer-sesji/src/app/components/register/register.component.ts
@@ -11,29 +11,27 @@ export class RegisterComponent implements OnInit {
   message: string
   regSubmitted= false
 
-  constructor(private formBuilder: FormBuilder) {
-    this.createForm();
-  }
+  constructor(private formBuilder: FormBuilder) {  }
 
-  createForm() {
-    this.reg = this.formBuilder.group({
-      login: [null, [Validators.required, Validators.minLength(3)]],
-      password: [null, [Validators.required, Validators.minLength(8)]],
-      type: ['BG', [Validators.required]],
-      name: [null, [Validators.required, Validators.pattern("[a-zA-ZęóąłśżźćńĘÓĄŚŁŻŹĆŃ]+$"), Validators.minLength(3)]],
-      system: [null, [Validators.required]],
-      experience: [null]
-    })
-  }
-
-  register(){
+  onSubmit(){
+    if (this.reg.valid){
     this.message = 'Spokojnie, rejestracja jeszcze się nie rozpoczęła :)'
-    this.regSubmitted = true
+    } else{
+      this.message = "Wypełnij wszystkie wymagane pola!"
+    }
   }
 
   ngOnInit() {
-    this.createForm();
-  }
+    this.reg = this.formBuilder.group({
+      login: [null, [Validators.required, Validators.pattern("[A-Za-z0-9]+$"), Validators.minLength(3)]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      type: ['BG', [Validators.required]],
+      name: [null, [Validators.required, Validators.pattern("[A-Za-zęóąłśżźćńĘÓĄŚŁŻŹĆŃ]+$"), Validators.minLength(3)]],
+      system: [null, [Validators.required]],
+      experience: [null]
+    })
+    this.reg.touched
+    }
   
   reg = new FormGroup({
     login: new FormControl(''),
