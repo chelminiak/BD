66ae6e2f88deedc96d6bc6493e99888eca235e5c
chelminiak <chelminiak@student.agh.edu.pl>
2021-01-10 20:58:06 +0100

    new components added

--- Planer-sesji/src/app/app.module.ts
+++ Planer-sesji/src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { TermComponent } from './components/term/term.component';
+import { TeamComponent } from './components/team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    TermComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
