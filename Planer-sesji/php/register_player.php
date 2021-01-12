b83fbeb52c159e9ad8197daad8b85dd82a07ae49
Karolz98 <44200387+Karolz98@users.noreply.github.com>
2021-01-12 19:53:20 +0100

    Create register_player.php

---
+++ Planer-sesji/php/register_player.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'connect.php';
+// Get the posted data.
+$postdata = file_get_contents("php://input");
+
+
+if(isset($postdata) && !empty($postdata))
+{
+  // Extract the data.
+  $request = json_decode($postdata);
+
+
+  // Validate.
+  //if(trim($request->data->model) === '' || (int)$request->data->price < 1)
+  //{
+  //return http_response_code(400);
+  //}
+
+    //$experience = $request->experience;
+    $login = $request->login;
+    $password = $request->password;
+    $system = $request->system;
+    $name = $request->name;
+    $email = $request->email;
+    $hashed_password = password_hash('$password', PASSWORD_DEFAULT);
+  // Store.
+  $sql = "INSERT INTO gracze (gracze.login,gracze.haslo, gracze.system,gracze.l_odbytych_sesji, gracze.imie,gracze.email) VALUES ('$login','$hashed_password','$system',0,'$name','$email')";
+
+
+  if(mysqli_query($con,$sql))
+  {
+    http_response_code(201);
+    echo "Records inserted successfully.";
+  }
+  else
+  {
+    http_response_code(422);
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+  }
+}
