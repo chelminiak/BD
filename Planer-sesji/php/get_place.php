f56cf8328fb3b2b43a295c6b2399cd25c059b2f2
Karolz98 <44200387+Karolz98@users.noreply.github.com>
2021-01-19 21:20:42 +0100

    Create get_place.php

---
+++ Planer-sesji/php/get_place.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Returns the list of cars.
+ */
+require 'connect.php';
+
+if(isset($postdata) && !empty($postdata))
+{
+  // Extract the data.
+//  $request = json_decode($postdata);
+
+  $login = //$request->login;
+  $sql = "SELECT id from mistrzowie where mistrzowie.login = '$login'";// pamietaj ze jest masa loginow identycznych aktualnie w bd, testowane dla id-mistrzowie = 3 - zwraca 3 wiersze i testowane dla login= 'acas' 1 wiersz
+
+  if($result = mysqli_query($con,$sql))
+  {
+    if (mysqli_num_rows($result) > 0)
+    {
+      while($row = mysqli_fetch_assoc($result))
+      {
+        $id_mistrzowie = $row['id'];
+      }
+     }
+     else
+     {
+        http_response_code(430);
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+     }
+   }
+
+  //$id_mistrzowie = 3;
+  $sql = "SELECT * from lokalizacja where lokalizacja.id_mistrzowie = '$id_mistrzowie'";
+  if($result = mysqli_query($con,$sql))
+  {
+    if (mysqli_num_rows($result) > 0)
+    {
+      $cr = 0;
+      while($row = mysqli_fetch_array($result))
+      {
+        //echo "cosik";
+        echo $row;
+        echo $row['id'];
+      }
+    }
+  }
+  else
+  {
+    http_response_code(404);
+  }
+}
