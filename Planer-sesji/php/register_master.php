97880e2dc5785596fd29b00a5e1aca44bf8b8eb8
Karolz98 <44200387+Karolz98@users.noreply.github.com>
2021-01-23 22:36:03 +0100

    literówka

--- Planer-sesji/php/register_master.php
+++ Planer-sesji/php/register_master.php
@@ -21,7 +21,7 @@ if(isset($postdata) && !empty($postdata))
     $email = $request->email;
     $price = 0;
     $hashed_password = password_hash('$password', PASSWORD_DEFAULT);
-  $sql = "INSERT INTO mistrzowie (mistrzowie.login,mistrzowie.haslo, mistrzowie.system, mistrzowie.system2, mistrzowie.system, mistrzowie.l_poprowadzonych, mistrzowie.imie, mistrzowie.oplata_za_sesje,mistrzowie.staz,mistrzowie.email) VALUES ('$login','$hashed_password','$system','$system2','$system3',0,'$name','$price','$experience','$email')";
+  $sql = "INSERT INTO mistrzowie (mistrzowie.login,mistrzowie.haslo, mistrzowie.system, mistrzowie.system2, mistrzowie.system3, mistrzowie.l_poprowadzonych, mistrzowie.imie, mistrzowie.oplata_za_sesje,mistrzowie.staz,mistrzowie.email) VALUES ('$login','$hashed_password','$system','$system2','$system3',0,'$name','$price','$experience','$email')";
 
 
   if(mysqli_query($con,$sql))
