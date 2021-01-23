37eb72d31731c9e6a533742dc914282437c3c9aa
Karolz98 <44200387+Karolz98@users.noreply.github.com>
2021-01-23 22:36:24 +0100

    Create add_team.php

---
+++ Planer-sesji/php/add_team.php
@@ -0,0 +1,50 @@
+<?php
+require_once 'connect.php';
+// Get the posted data.
+$postdata = file_get_contents("php://input");
+
+
+if(isset($postdata) && !empty($postdata))
+{
+  // Extract the data.
+  $request = json_decode($postdata);
+
+  $login = $request->login;
+  $sql = "SELECT id from mistrzowie where mistrzowie.login = '$login'";
+
+  if($result = mysqli_query($con,$sql))
+  {
+    if (mysqli_num_rows($result) > 0)
+    {
+      while($row = mysqli_fetch_assoc($result))
+      {
+        $id_mistrzowie = $row['id'];
+
+      }
+     }
+     else
+     {
+        http_response_code(430);
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+     }
+   }
+
+    $num_people = $request->num_people;
+    $max_people = $request->max_people;
+    $system = $request->system;
+    $num_sessions = $request->num_sessions;
+    $name = $request->name;
+    $sql = "INSERT INTO druzyna (druzyna.id_mistrzowie,druzyna.l_czlonkow,druzyna.max_l_czlonkow,druzyna.system,druzyna.l_odbytych_sesji,druzyna.nazwa) VALUES ('$id_mistrzowie','$num_people','$max_people','$system','$num_sessions','$name')";
+
+   //true podawac jako 1, false = 0
+  if(mysqli_query($con,$sql))
+  {
+    http_response_code(201);
+    echo "Records inserted successfully.";
+  }
+  else
+  {
+    http_response_code(422);
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+  }
+}
