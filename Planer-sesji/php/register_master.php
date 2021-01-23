6e370400729f14892bb7237bb7cdee93a1b6c1a6
Karolz98 <44200387+Karolz98@users.noreply.github.com>
2021-01-23 21:52:15 +0100

    więcej systemów

--- Planer-sesji/php/register_master.php
+++ Planer-sesji/php/register_master.php
@@ -10,18 +10,18 @@ if(isset($postdata) && !empty($postdata))
   $request = json_decode($postdata);
 
 
- 
 
     $experience = $request->experience;
     $login = $request->login;
     $password = $request->password;
     $system = $request->system;
+    $system2 = $request->system2;
+    $system3 = $request->system3;
     $name = $request->name;
     $email = $request->email;
     $price = 0;
     $hashed_password = password_hash('$password', PASSWORD_DEFAULT);
-  
-  $sql = "INSERT INTO mistrzowie (mistrzowie.login,mistrzowie.haslo, mistrzowie.system,mistrzowie.l_poprowadzonych, mistrzowie.imie, mistrzowie.oplata_za_sesje,mistrzowie.staz,mistrzowie.email) VALUES ('$login','$hashed_password','$system',0,'$name','$price','$experience','$email')";
+  $sql = "INSERT INTO mistrzowie (mistrzowie.login,mistrzowie.haslo, mistrzowie.system, mistrzowie.system2, mistrzowie.system, mistrzowie.l_poprowadzonych, mistrzowie.imie, mistrzowie.oplata_za_sesje,mistrzowie.staz,mistrzowie.email) VALUES ('$login','$hashed_password','$system','$system2','$system3',0,'$name','$price','$experience','$email')";
 
 
   if(mysqli_query($con,$sql))
