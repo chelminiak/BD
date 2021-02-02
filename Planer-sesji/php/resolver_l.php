3468c97246db7962af83fad2bdfbec13bf60bfdf
Karolz98 <44200387+Karolz98@users.noreply.github.com>
2021-02-02 19:02:37 +0100

    Create resolver_l.php

---
+++ Planer-sesji/php/resolver_l.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'connect.php';
+$postdata = file_get_contents("php://input");
+if(isset($postdata) && !empty($postdata))
+{
+  // Extract the data.
+  $request = json_decode($postdata);
+
+  $id= $request->id;
+  $sql = "SELECT adres,miasto from lokalizacja l where l.id =?";
+
+  if($stmt = mysqli_prepare($con,$sql))
+  {
+     mysqli_stmt_bind_param($stmt, "s", $id);
+     mysqli_stmt_bind_result($stmt, $address, $city); //dodac po przecinku kolejne paramtry jak chcesz zwrocic wiecej niz 1
+     mysqli_stmt_execute($stmt);
+     mysqli_stmt_store_result($stmt);
+     if (mysqli_stmt_num_rows($stmt) > 0)
+     {
+         mysqli_stmt_fetch($stmt);
+         mysqli_stmt_close($stmt);
+         $data = [$address,$city]; //tak zwaracac wiecej niz jedno pole
+         echo json_encode(['data'=>$data]);
+
+     }
+     else
+     {
+        http_response_code(420);
+     }
+   }
+   else
+   {
+      http_response_code(404);
+   }
+}
