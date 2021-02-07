69b00eb1a3a59b66bd6c2e8070f3da80ab4e25ea
Karolz98 <44200387+Karolz98@users.noreply.github.com>
2021-02-07 18:55:36 +0100

    profiles updates

--- Planer-sesji/php/update_profile.php
+++ Planer-sesji/php/update_profile.php
@@ -1,4 +1,4 @@
-<?php
+?php
 
 require 'connect.php';
 $postdata = file_get_contents("php://input");
@@ -6,18 +6,20 @@ if(isset($postdata) && !empty($postdata))
 {
   // Extract the data.
   $request = json_decode($postdata);
-  //Brakuje walidacji czy dana zostala wyslana
-  //na razie traktuje ze wszystkie poszly
-  $login = $request->login;
+
+  $request = json_decode($postdata);
+  $experience = $request->experience;
   $system = $request->system;
   $system2 = $request->system2;
   $system3 = $request->system3;
-  $imie = $request->imie;
-  $staz = $request->staz;
+  $name = $request->name;
   $email = $request->email;
-  $miasto = $request->miasto;
+  $city = $request->city;
+  $money = $request->money;
+  $min = $request->min;
+  $login = $request->login;
 
-  $sql = "UPDATE gracze g SET g.system = '$system',g.system2 = '$system2',g.system3 = '$system3',g.imie = '$imie',g.staz = '$staz',g.email = '$email',g.miasto = '$email' WHERE g.login =?";
+  $sql = "UPDATE gracze g SET g.system = '$system',g.system2 = '$system2',g.system3 = '$system3',g.imie = '$name',g.staz = '$experience',g.email = '$email',g.miasto = '$city' WHERE g.login = ?";
   if($stmt = mysqli_prepare($con,$sql))
   {
      mysqli_stmt_bind_param($stmt, "s", $login);
@@ -25,12 +27,33 @@ if(isset($postdata) && !empty($postdata))
      if(mysqli_stmt_affected_rows($stmt) > 0)
      {
          http_response_code(201);
+         mysqli_stmt_close($stmt);
      }
      else
      {
-        http_response_code(420);
+        $sql = "UPDATE mistrzowie m SET m.system = '$system', m.system2 = '$system2', m.system3 = '$system3', m.imie = '$name', m.oplata_za_sesje = '$money', m.staz = '$experience', m.minimalny_staz_gracza = '$min', m.email = '$email', m.miasto = '$city' WHERE m.login = ?";
+        if($stmt = mysqli_prepare($con,$sql))
+        {
+           mysqli_stmt_bind_param($stmt, "s", $login);
+           mysqli_stmt_execute($stmt);
+           if(mysqli_stmt_affected_rows($stmt) > 0)
+           {
+              http_response_code(201);
+              mysqli_stmt_close($stmt);
+           }
+           else
+           {
+         //     echo $sql;
+        //      echo mysqli_error($con);
+               mysqli_stmt_close($stmt);
+               http_response_code(420);
+           }
+        }
+        else
+        {
+           http_response_code(404);
+        }
      }
-        mysqli_stmt_close($stmt);
    }
    else
    {
